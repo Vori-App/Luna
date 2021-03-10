ee8538336981429fe88fe0e4a8d80e0ca5c4fcae
ArcherDevelopment <72310084+ArcherDevelopment@users.noreply.github.com>
2021-03-10 12:55:37 -0500

    Add files via upload

---
+++ Luna-loader.lua
@@ -0,0 +1,14 @@
+  
+local games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArcherDevelopment/Luna/main/luna-games.lua"))()
+local ingame = false
+for i,gam in pairs(games) do
+  if game.PlaceId == gam.ID then
+     ingame = true
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/ArcherDevelopment/Luna/main/games/"..gam.Script))()
+ else
+     ingame = false
+  end
+end
+if ingame == false then
+ error("HEY! YOUR NOT IN A SUPPORTED GAME!")
+end
