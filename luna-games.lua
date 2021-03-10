63e56b58cefa0ea2e7cc80c896f1180c2da23336
ArcherDevelopment <72310084+ArcherDevelopment@users.noreply.github.com>
2021-03-10 13:19:19 -0500

    Update luna-games.lua

--- luna-games.lua
+++ luna-games.lua
@@ -1,6 +1,4 @@
 local games = {
  ["13822889"] = {Name = "Lumber Tycoon 2", Script = "LumberTycoon.lua",ID = 13822889}
- ["443406476"] = {Name = "Project Lazarus ZOMBIES NEW LAUNCHER", Script = "Obfuscated.lua",ID = 443406476}
-
 }
 return games;
