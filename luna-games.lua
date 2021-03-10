5562bc317130a3ecec2e366273096749ad343c3b
ArcherDevelopment <72310084+ArcherDevelopment@users.noreply.github.com>
2021-03-10 14:21:28 -0500

    Update luna-games.lua

--- luna-games.lua
+++ luna-games.lua
@@ -1,4 +1,4 @@
 local games = {
-  ['4032944086'] = {Name = "Lumber-Tycoon-2", Script = "LumberTycoon.lua",ID = 4032944086}
+  ['4032944086'] = {Name = "Lumber Tycoon 2", Script = "LumberTycoon.lua",ID = 4032944086}
  }
 return games;
